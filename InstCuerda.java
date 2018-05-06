95da3015f055fc9fb835c19643b2549958790431
ErickG4rci4 <a01740197@itesm.mx>
2018-05-06 11:59:12 -0500

    Cambio de Cuerda por InstCuerda

---
+++ InstCuerda.java
@@ -0,0 +1,12 @@
+class InstCuerda extends Instrumentos{
+	private Cuerdas misCuerdas;
+	public String tipo;
+	public String material;
+	//Constructor
+	Cuerda(String nombre, String marca, int fechaFabricacion, boolean electrico,String tipo, String material, String cmaterial, String cmarca , String estado, boolean afinado){
+		super(nombre,marca,fechaFabricacion,electrico);
+		this.tipo = tipo;
+		this.material = material;
+		//misCuerdas = new Cuerdas(cmaterial,cmarca,estado,afinado);
+	}
+}
