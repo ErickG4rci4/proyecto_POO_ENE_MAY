8761ca83ba393b2950c627aa0d5a7cee56359261
ErickG4rci4 <a01740197@itesm.mx>
2018-05-06 20:13:01 -0500

    cargar

--- Archivo.java
+++ Archivo.java
@@ -12,9 +12,13 @@ class Archivo implements Serializable{
 	}
 
 	public static <T> void guardar(ArrayList<T> inputArray ){
-		String tmp;
-		File archivo = new File("Data.ser");
+		String tmp, line;
+		boolean t = new File("Data.txt").exists();
+		//if (t = false)
+			File archivo = new File("Data.txt");
 		try {
+			//BufferedReader read = new BufferedReader(new FileReader("Data.txt"));
+			//line = read.readLine();
 			FileOutputStream arch = new FileOutputStream(archivo);
 			ObjectOutputStream Obj = new ObjectOutputStream(arch);
 			for (T element : inputArray){
@@ -28,4 +32,37 @@ class Archivo implements Serializable{
 		}
 	}
 
+	public static <T> ArrayList cargar(){
+		String line;
+		ArrayList<T> ArrayInst = null; 
+		T Obj = null;
+		try {
+		BufferedReader read = new BufferedReader(new FileReader("Data.txt"));
+		line = read.readLine();
+		FileInputStream uno = null;
+		ObjectInputStream dos = null;
+		if (line == null){
+			File archivo = new File("Data.txt");
+		}
+		else {
+			int tmp = Integer.parseInt(line);
+			for(int i = 0; i<tmp; i++){
+				uno = new FileInputStream("Data.txt");
+				dos = new ObjectInputStream(uno);
+				Obj = (T) dos.readObject();
+				System.out.println(Obj);			
+				ArrayInst.add(Obj);
+			}
+		}
+	}
+	catch (IOException e) {
+    	System.err.println(e);
+    	System.exit(1);
+	}
+	catch (ClassNotFoundException e){
+		System.err.println(e);
+	}
+	return ArrayInst;
+	}
+	
 }
